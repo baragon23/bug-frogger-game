1e81ad6f97bdae2e58c71e183a16eced9b427849
Benjamin Aragon <baragon23@users.noreply.github.com>
2018-10-05 12:26:50 -0700

    dt multiplication
    smoother speed across all computers with multiplication of DT constant

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ var Enemy = function(x, y) {
 	this.y = y;
 	this.width = 75;
 	this.height = 50;
-	this.speed = Math.floor(Math.random() * 4 + 1);
+	this.speed = Math.floor(Math.random() * 600 + 1);
 };
 
 // Update the enemy's position, required method for game
@@ -20,7 +20,7 @@ Enemy.prototype.update = function(dt) {
 	// You should multiply any movement by the dt parameter
 	// which will ensure the game runs at the same speed for
 	// all computers.
-	this.x += this.speed;
+	this.x += this.speed * dt;
 
 	// enemy made it to the edge of the screen so they get reset back to beginning
 	if (this.x > 510) {
